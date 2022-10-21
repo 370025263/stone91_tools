da3ed4664bf3f7ee1f26d34c8ec7f5587b1f3eb7
370025263 <370025263@qq.com>
2022-10-21 18:10:23 +0800

    upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages            #这个包没有的可以
 
 setup(
     name = "stone91_tools",      #这里是pip项目发布的名称
-    version = "0.0.1",  #版本号，数值大的会优先被pip
+    version = "0.0.2",  #版本号，数值大的会优先被pip
     keywords = ["pip", "stone91_tools"],			# 关键字
     description = "stone91's private tools.",	# 描述
     long_description = "includes plotting ml imageprocessing etc.",
